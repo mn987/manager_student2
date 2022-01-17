b4f439079331d081901b013bb0ac2b181e779564
mn987 <73776095+mn987@users.noreply.github.com>
2022-01-17 22:11:47 +0700

    Update Student.py

--- Data/Student.py
+++ Data/Student.py
@@ -1,3 +1,9 @@
+class People:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self. age = age
+        self. gender = gender
+
 class Student:
     def __init__(self, name, age, gender, math, literature, english, gpa, rank ,id):
         self.name = name
@@ -9,3 +15,17 @@ class Student:
         self.gpa = gpa
         self.rank = rank
         self.id = id
+
+class Teacher:
+    def __init__(self, name, age, gender, subject,id):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.subject = subject
+        self.id = id
+
+class Subjects:
+    def __init__(self, math, literature, english):
+        self.math = math
+        self.literature = literature
+        self.english = english
